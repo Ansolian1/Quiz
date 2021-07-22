deb5dabc52e8ec88fde9be1e96f860b9814eaf4b
Ansolian <soldatov.v.v1997@gmail.com>
2021-07-22 20:10:06 +0900

    Cell effects

---
+++ Quiz/Assets/Scripts/Cell.cs
@@ -0,0 +1,40 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cell : MonoBehaviour
+{
+    [SerializeField] private Image cellContents;
+    [HideInInspector] public int CurrentNumber;
+    [HideInInspector] public Image CurrentImage;
+
+    void Start()
+    {
+    }
+
+    public void WrongAnswerClick()
+    {
+        EaseInBounceEffect(cellContents.transform, 1f);
+    }
+
+    public void RightAnswerClick()
+    {
+        BounceObjectEffect(cellContents.transform);
+    }
+
+    private void BounceObjectEffect(Transform transform)
+    {
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(transform.DOScale(new Vector2(0f, 0f), 0));
+        sequence.Append(transform.DOScale(new Vector2(1.2f, 1.2f), 0.2f));
+        sequence.Append(transform.DOScale(new Vector2(0.95f, 0.95f), 0.2f));
+        sequence.Append(transform.DOScale(new Vector2(1f, 1f), 0.2f));
+    }
+
+    private void EaseInBounceEffect(Transform transform, float effectTime)
+    {
+        transform.DOShakePosition(effectTime, 3);
+    }
+}
